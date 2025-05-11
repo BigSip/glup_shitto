acd9419b7b5105c03a9b76bc63fb69616936dc48
BigSip <mzuwala@outlook.com>
2025-05-11 00:11:43 -0400

    implemented music

--- main_menu.gd
+++ main_menu.gd
@@ -1,6 +1,5 @@
 extends Node2D
 
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -10,15 +9,18 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
-
+var music = true
 
 func _on_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://GlupHouse.tscn")
+	if music == true:
+		Bgmusic.get_tree().get_first_node_in_group("bgm").play()
+	else:
+		pass
 
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
 
-
-func _on_touch_screen_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://GlupHouse.tscn")
+func _on_music_toggled(toggled_on: bool) -> void:
+	music = false
