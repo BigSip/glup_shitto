f148e7463be43e5876643c46a8b25ae74beed391
BigSip <mzuwala@outlook.com>
2025-05-30 23:23:22 -0400

    1.0, game released.

--- navman.gd
+++ navman.gd
@@ -1,14 +1,18 @@
 extends Node
 
+const scene_mm = preload("res://main_menu.tscn")
 const scene_glup_house = preload("res://GlupHouse.tscn")
 const scene_glup_yard = preload("res://GlupFrontYard.tscn")
 const scene_glup_hills = preload("res://GlupHills.tscn")
-const scene_demo = preload("res://GlupDemoVideo.tscn")
 const scene_picking = preload("res://potatoPicking.tscn")
 const scene_house2 = preload("res://GlupHouse2.tscn")
 const scene_ihmcut = preload("res://hatemyselfcutscene.tscn")
 const scene_dream = preload("res://dream.tscn")
 const scene_house3 = preload("res://house3.tscn")
+const scene_gfy2 = preload("res://GlupFrontYard2.tscn")
+const scene_climb = preload("res://climb1.tscn")
+const scene_summit = preload("res://summit.tscn")
+const scene_end = preload("res://end.tscn")
 
 signal on_trigger_player_spawn
 
@@ -18,14 +22,14 @@ func go_to_level(level_tag, destination_tag):
 	var scene_to_load
 	
 	match level_tag:
+		"mm":
+			scene_to_load = scene_mm
 		"GlupHouse":
 			scene_to_load = scene_glup_house
 		"GlupYard":
 			scene_to_load = scene_glup_yard
 		"GlupHills":
 			scene_to_load = scene_glup_hills
-		"Demo":
-			scene_to_load = scene_demo
 		"picking":
 			scene_to_load = scene_picking
 		"house2":
@@ -36,7 +40,15 @@ func go_to_level(level_tag, destination_tag):
 			scene_to_load = scene_dream
 		"house3":
 			scene_to_load = scene_house3
-			
+		"gfy2":
+			scene_to_load = scene_gfy2
+		"climb":
+			scene_to_load = scene_climb
+		"summit":
+			scene_to_load = scene_summit
+		"end":
+			scene_to_load = scene_end
+
 	if scene_to_load != null:
 		spawn_door_tag = destination_tag
 		get_tree().change_scene_to_packed(scene_to_load)
